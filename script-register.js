e001e04fbdc5be65d0d4e7c58e783bd2522e5b93
EnBest2 <enbest2@gmail.com>
2025-05-17 15:00:32 +0200

    Create script-register.js

---
+++ script-register.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const registerBtn = document.getElementById('register-btn');
+  registerBtn.addEventListener('click', async () => {
+    const username = document.getElementById('register-username').value.trim();
+    const password = document.getElementById('register-password').value;
+    const res = await fetch('/api/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username, password })
+    });
+    const data = await res.json();
+    if (data.error) {
+      alert(data.error);
+    } else {
+      alert(data.message);
+      window.location.href = 'login.html';
+    }
+  });
+});
